c2959f04cff918fc1d2d4c06911f7ebf82e0f43c
volkovask <volkov.ask@yandex.ru>
2022-07-09 18:32:04 +0700

    test: version 1.0

--- src/test/java/notation/ReversePolishNotationCalculatorTest.java
+++ src/test/java/notation/ReversePolishNotationCalculatorTest.java
@@ -1,11 +1,28 @@
 package notation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ReversePolishNotationCalculatorTest {
 
+    ReversePolishNotationCalculator reversePolishNotationCalculator =
+             new ReversePolishNotationCalculator();
+
     @Test
     public void shouldCalculateAddition() {
-
+        Assertions.assertEquals(9, reversePolishNotationCalculator.calculatePolishNotation("5 4 +"));
     }
+
+    @Test
+    public void shouldCalculateSubtraction() {
+        int result = reversePolishNotationCalculator.calculatePolishNotation("4 3 -");
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    public void shouldCalculateMultiplication() {
+        int result = reversePolishNotationCalculator.calculatePolishNotation("5    4 -3 * *");
+        Assertions.assertEquals(-60, result);
+    }
+
 }
